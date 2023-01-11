b796da459e37985085793cdaf601849997e47e5f
ohdaira <ohdaira@beingcorp.co.jp>
2023-01-11 10:50:59 +0900

    chapter3の演習問題を提出します。
    大問２問でそれぞれ小問が４問ありました。

---
+++ chapter3/Question3-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//問題3-1
+//以下のリストが定義してあります。
+//var number = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+//このリストに対して、ラムダ式を使い、次のコードを書いてください。
+
+//1. List<T>のExistsメソッドを使い、8か9で割り切れる数字があるかどうかを調べ、
+//その結果をコンソールに出力してください。
+
+//2.　List<T>のForEachメソッドを使い、各要素を2.0で割った値をコンソールに出力してください。
+
+//3. LINQのWhereメソッドを使い、値が50以上の要素を列挙し、その結果をコンソールに出力してください。
+
+//4. LINQのSelectメソッドを使い、それぞれの値を2倍にし、その結果をList<int>に格納してください。
+//その後、List<int>の各要素をコンソールに出力してください。
+
+namespace Question3_1 {
+    class Program {
+        static void Main(string[] args) {
+            //1.の回答
+            var number = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+            var exists = number.Exists(n => n % 8 == 0 || n % 9 == 0);
+            Console.WriteLine(exists);
+
+            //2.の回答
+            number.ForEach(s => Console.WriteLine(s / 2.0));
+
+            //3. の回答
+            var more50num = number.Where(n => n >= 50);
+            foreach (var s in more50num)
+                Console.WriteLine(s);
+
+            //4. の回答
+            var twicenum = number.Select(n => n * 2);
+            foreach (int n in twicenum)
+                Console.WriteLine(n);
+        }
+    }
+}
---
+++ chapter3/Question3-2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//問題3-2
+//以下のリストが定義してあります。
+//var name = new List<string>{
+//    "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+//};
+//このリストに対して、ラムダ式を使い、次のコードを書いてください。
+
+//1. コンソールから入力した都市名が何番目に格納されているかList<T>のFindIndexメソッドを使って調べ、
+//その結果をコンソールに出力してください。
+//見つからなかったら、-1を出力してください。
+//なお、コンソールからの入力には、Console.ReadLineメソッドを利用してください。
+//var line = Console.ReadLine();
+
+//2. LINQのCountメソッドを使い、小文字の'o'が含まれている都市名がいくつあるかカウントし、
+//その結果をコンソールに出力してください。
+
+//3. LINQのWhereメソッドを使い、小文字の'o'が含まれている都市名を抽出し、配列に格納してください。
+//その後、配列の各要素をコンソールに出力してください。
+
+//4. LINQのWhereメソッドとSelectメソッドを使い、'B'で始まる都市名の文字数を抽出し、
+//その文字数をコンソールに出力してください。都市名を表示する必要は、ありません。
+namespace Question3_2 {
+    class Program {
+        static void Main(string[] args) {
+            var name = new List<string>{
+            "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+            //1. の回答
+            var line = Console.ReadLine();
+            int index = name.FindIndex(s => s == line);
+            if (index > 0) {
+                Console.WriteLine(index);
+            } else {
+                Console.WriteLine(-1);
+            }
+
+            //2. の回答
+            var count = name.Count(s => s.Contains('o'));
+            Console.WriteLine(count);
+
+            //3. の回答
+            string[] containnum = name.Where(s => s.Contains('o')).ToArray();
+            foreach(var s in containnum)
+                Console.WriteLine(s);
+
+            //4. の回答
+            var firstBstring = name.Where(s => s.Substring(0,1) == "B").Select(s => s.Length);
+            foreach(var s in firstBstring)
+                Console.WriteLine(s);
+        }
+    }
+}
