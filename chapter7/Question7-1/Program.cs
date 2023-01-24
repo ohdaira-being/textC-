06ffc888862fc501424c49a77471df09f6dbba31
大平 強（OHDAIRA Tsuyoshi） <ohdaira@beingcorp.co.jp>
2023-01-24 11:42:03 +0900

    7章　章末問題

---
+++ chapter7/Question7-1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Question7_1 {
+    //"Cozy lummox gives smart squid who asks for job pen"という文字列があります。
+    //この文字列に対して、以下のコードを書いてください。
+
+    //1.各アルファベット文字（空白などアルファベット意外は除外）が何文字ずつ現れるかカウントするプログラムを書いてください。
+    //このときに、必ずディクショナリクラスを使ってください。
+    //大文字/小文字の区別はしないでください。
+    //以下の形式で出力してください。
+    //'A':2
+    //'B':1
+    //'C':1
+    //'D':1
+    //  :
+    //  :
+
+    //2.上記プログラムを、SortedDictionary<TKey, TValue>を使って書き換えてください。
+
+    class Program {
+        static void Main(string[] args) {
+            //1.の回答
+            Console.WriteLine("～1問目の回答～");
+            string wText = "Cozy lummox gives smart squid who asks for job pen".Replace(" ", "").ToUpper();
+            var wTextDict = new Dictionary<char, int>();
+            foreach (char wWord in wText.Distinct()) {
+                wTextDict[wWord] = wText.Count(x => x == wWord);
+            }
+            foreach (KeyValuePair<char, int> wDict in wTextDict.OrderBy(x => x.Key)) {
+                Console.WriteLine($"'{wDict.Key}'：{wDict.Value}");
+            }
+
+            //2.の回答
+            Console.WriteLine("～2問目の回答～");
+            var wRewriteTextDict = new SortedDictionary<char, int>();
+            foreach (char wWord in wText.Distinct()) {
+                wRewriteTextDict[wWord] = wText.Count(x => x == wWord);
+            }
+            foreach (KeyValuePair<char, int> wDict in wRewriteTextDict) {
+                Console.WriteLine($"'{wDict.Key}'：{wDict.Value}");
+            }
+        }
+    }
+}
---
+++ chapter7/Question7-2/Abbreviations.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Question7_2 {
+    /// <summary>
+    /// 略語と対応する日本語を管理するクラス
+    /// </summary>
+    class Abbreviations {
+        private Dictionary<string, string> wDict = new Dictionary<string, string>();
+
+        //1.の回答
+        public int Count => wDict.Count();
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Abbreviations() {
+            var wLines = File.ReadAllLines("Abbreviations.txt");
+            wDict = wLines.Select(line => line.Split('=')).ToDictionary(x => x[0], x => x[1]);
+        }
+
+        //2.の回答
+        public bool Remove(string vRemoveText) {
+            return wDict.Remove(vRemoveText);
+        }
+
+        /// <summary>
+        /// 要素を追加
+        /// </summary>
+        /// <param name="vAbbr">ディクショナリwDictに追加するキー</param>
+        /// <param name="vJapanese">ディクショナリwDictに追加する値</param>
+        public void Add(string vAbbr, string vJapanese) {
+            wDict[vAbbr] = vJapanese;
+        }
+
+        /// <summary>
+        /// インデクサ
+        /// </summary>
+        /// <param name="vAbbr">検索するキー</param>
+        /// <returns>ディクショナリwDictのキー[vAbbr]に対応する値。なければ、nullを返す。</returns>
+        public string this[string vAbbr] {
+            get {
+                return wDict.ContainsKey(vAbbr) ? wDict[vAbbr] : null;
+            }
+        }
+
+        /// <summary>
+        /// 日本語から対応する省略語を取り出す
+        /// </summary>
+        /// <param name="vJapanese">検索する値</param>
+        /// <returns>ディクショナリwDictの値[vJapanese]に対応する値。なければ、nullを返す。</returns>
+        public string ToAddreviations(string vJapanese) {
+            return wDict.FirstOrDefault(x => x.Value == vJapanese).Key;
+        }
+
+        /// <summary>
+        /// 日本語の位置を引数に与え、それが含まれる要素(Key, Value)を全て取り出す
+        /// </summary>
+        /// <param name="vSubstring">検索するワード</param>
+        /// <returns>値に検索ワードを含む全てのディクショナリの要素を返す。</returns>
+        public IEnumerable<KeyValuePair<string, string>> FindAll(string vSubstring) {
+            foreach (var wItem in wDict) {
+                if (wItem.Value.Contains(vSubstring))
+                    yield return wItem;
+            }
+        }
+
+        /// <summary>
+        /// 追加したメソッド。文字数を引数に取り、その文字数のKeyを持ったディクショナリの要素を返す。
+        /// </summary>
+        /// <param name="vKeyCharNum">文字数int</param>
+        /// <returns>指定された文字数のKeyを持ったディクショナリの要素を全て返す。</returns>
+        public IEnumerable<KeyValuePair<string, string>> FindKeyAllAtNum(int vKeyCharNum) {
+            foreach (KeyValuePair<string, string> wItemAtNum in wDict.Where(x => x.Key.Length == vKeyCharNum)) {
+                yield return wItemAtNum;
+            }
+        }
+    }
+}
