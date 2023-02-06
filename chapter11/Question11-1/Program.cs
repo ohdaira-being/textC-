c7de0ceb058e952e92e29d55a3d1f42da217bb92
大平 強（OHDAIRA Tsuyoshi） <ohdaira@beingcorp.co.jp>
2023-02-06 14:01:52 +0900

    11章　章末問題

---
+++ chapter11/Question11-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Question11_1 {
+
+    //chapter11で作成したXMLファイルに対して、1～4のコードを書いてください。
+    //1.XMLファイルを読み込み、競技名とチームメンバー数の一覧を表示してください。
+    //2.最初にプレイされた年の若い順に漢字の競技名を表示してください。
+    //3.メンバー人数が最も多い競技名を表示してください。
+    //4.サッカーの情報を追加して、新たなXMLファイルに出力してください。ファイル名は特に問いません。
+
+    class Program {
+        static void Main(string[] args) {
+            XDocument wXdoc = XDocument.Load("../../../../BallSports.xml");//ファイルが見つからないと例外になってしまう
+
+            //1.の回答
+            foreach (XElement wXlist in wXdoc.Root.Elements()) {
+                Console.WriteLine($"競技名：{wXlist.Element("name").Value}→{wXlist.Element("teammenbers").Value}人");
+            }
+
+            //2.の回答
+            foreach (XElement wXListOrderByFirstplayed in wXdoc.Root.Elements().OrderBy(x => (int)(x.Element("firstplayed")))) {
+                Console.WriteLine($"{wXListOrderByFirstplayed.Element("name").Attribute("kanji").Value}");
+            }
+
+            //3.の回答
+            foreach (XElement wXMaxMenberSport in wXdoc.Root.Elements().OrderByDescending(x => (int)(x.Element("teammenbers"))).Take(1)) {
+                Console.WriteLine(wXMaxMenberSport.Element("name").Value);
+            }
+
+            //4.の回答
+            XElement wSoccerElement = new XElement("ballsport",
+                       new XElement("name", "フットボール", new XAttribute("kanji", "蹴球")),
+                       new XElement("teammenbers", "11"),
+                       new XElement("firstplayed", "1863")
+            );
+            wXdoc.Root.Add(wSoccerElement);
+            wXdoc.Save("../../../../BallSports.xml");
+        }
+    }
+}
---
+++ chapter11/Question11-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Question11_2 {
+    //Sample11-2.xmlを次の形式に変換し、別のファイルに保存してください。
+    //<? xml version="1.0" encoding="utf-8"?>
+    //<difficultkanji>
+    //    <word kanji = "鬼灯" yomi="ほおずき" />
+    //    <word kanji = "暖簾" yomi="のれん" />
+    //    <word kanji = "杜撰" yomi="ずさん" />
+    //    <word kanji = "坩堝" yomi="るつぼ" />
+    //</difficultkanji>
+    class Program {
+        static void Main(string[] args) {
+            var wXdoc = XDocument.Load("../../../../Sample11-2.xml");
+            var wPairs = wXdoc.Root.Elements().Select(x => new {
+                Key = x.Element("kanji").Value,
+                Value = x.Element("yomi").Value
+            });
+            var wDict = wPairs.ToDictionary(x => x.Key, x => x.Value);
+            var wXNewDoc = new XElement("difficultkanji",
+                wDict.Select(x => new XElement("word",
+                    new XAttribute("kanji", x.Key),
+                    new XAttribute("yomi", x.Value)
+                ))
+            );
+            wXNewDoc.Save("../../../../NewSample11-2.xml");
+        }
+    }
+}
