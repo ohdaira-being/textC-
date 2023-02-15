16ad5015bcc2731cbad216d26d3f40335015f435
大平 強（OHDAIRA Tsuyoshi） <ohdaira@beingcorp.co.jp>
2023-02-15 17:34:20 +0900

    11章　章末問題
    ・BallSportクラスの見直し
    ・メソッド名・変数名の見直し
    ・ファイルへの追加方法の修正

---
+++ chapter11/Question11-1/BallSport.cs
@@ -0,0 +1,58 @@
+﻿using System.Xml.Linq;
+
+namespace Question11_1 {
+
+    /// <summary>
+    /// BallSportクラス
+    /// </summary>
+    public class BallSport {
+
+        /// <summary>
+        /// 競技名プロパティ
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 競技名の漢字名称プロパティ
+        /// </summary>
+        public string KanjiName { get; set; }
+
+        /// <summary>
+        /// チーム人数プロパティ
+        /// </summary>
+        public int Teammembers { get; set; }
+
+        /// <summary>
+        /// 最初にプレイした年プロパティ
+        /// </summary>
+        public int Firstplayed { get; set; }
+
+        /// <summary>
+        /// BallSportのコンストラクタ
+        /// </summary>
+        /// <param name="vName">競技名</param>
+        /// <param name="vKanjiName">競技名の漢字名称</param>
+        /// <param name="vTeammembers">チーム人数</param>
+        /// <param name="vFirstplayed">最初にプレイした年</param>
+        public BallSport(string vName, string vKanjiName, int vTeammembers, int vFirstplayed) {
+            this.Name = vName;
+            this.KanjiName = vKanjiName;
+            this.Teammembers = vTeammembers;
+            this.Firstplayed = vFirstplayed;
+        }
+
+        // BallSportのインスタンスを受け取り、XElement型を返すメソッド
+        /// <summary>
+        /// BallSportインスタンスを受け取り、XElement型にして返すメソッド
+        /// </summary>
+        /// <param name="vBallSport">BallSport</param>
+        /// <returns></returns>
+        public static XElement ToXElement(BallSport vBallSport) {
+            return new XElement("ballsport",
+                       new XElement("name", vBallSport.Name, new XAttribute("kanji", vBallSport.KanjiName)),
+                       new XElement("teammembers", vBallSport.Teammembers),
+                       new XElement("firstplayed", vBallSport.Firstplayed)
+            );
+        }
+    }
+}
--- chapter11/Question11-1/Program.cs
+++ chapter11/Question11-1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace Question11_1 {
 
@@ -12,47 +13,93 @@ namespace Question11_1 {
     //4.サッカーの情報を追加して、新たなXMLファイルに出力してください。ファイル名は特に問いません。
 
     class Program {
+
+        /// <summary>
+        /// vFilePathを読み込み、全てのBallsportのプロパティを配列にして返すメソッド
+        /// </summary>
+        /// <param name="vFilePath">ファイルのパス</param>
+        /// <returns>読み込んだファイルの全てのBallSportのプロパティを配列化</returns>
+        public static IEnumerable<BallSport> ReadXFileToBallsports(string vFilePath) {
+
+            // ファイルが見つからないと例外になってしまう
+            XDocument wXdoc = XDocument.Load(vFilePath);
+
+            BallSport[] wReadedBallSports = wXdoc.Root.Elements()
+                                            .Select(x => new BallSport(
+                                                 (string)x.Element("name"),
+                                                 (string)x.Element("name").Attribute("kanji"),
+                                                 (int)x.Element("teammembers"),
+                                                 (int)x.Element("firstplayed")
+                                            )).ToArray();
+            return wReadedBallSports;
+        }
+
+        // BallsportListに書き込む。
+        /// <summary>
+        /// vFilePathに追加で書き込むメソッド
+        /// </summary>
+        /// <param name="vBallSportData">追加するBallSport</param>
+        /// <param name="vFilePath">書き込み先のファイルパス</param>
+        public static void AddBallSport(BallSport vBallSportData, string vFilePath) {
+
+            // BallSports.xmlファイルがなければ、作成する。
+            if (!File.Exists(vFilePath)) {
+                var wBallSports = new XElement("ballsports", "");
+                XDocument aaa = new XDocument(wBallSports);
+                aaa.Save(vFilePath);
+            }
+
+            XElement wAddData = new XElement(BallSport.ToXElement(vBallSportData));
+            XDocument wXdoc = XDocument.Load(vFilePath);
+
+            wXdoc.Root.Add(wAddData);
+            wXdoc.Save(vFilePath);
+        }
+
         static void Main(string[] args) {
+
+            var wBasketball = new BallSport("バスケットボール", "籠玉", 5, 1891);
+            var wVolleyball = new BallSport("バレーボール", "排球", 6, 1895);
+            var wBaseball = new BallSport("ベースボール", "野球", 9, 1846);
 
             string wFilePath = "../../../../BallSports.xml";
 
+
             // BallSports.xmlファイルがなければ、作成する。
             Console.WriteLine("BallSports.xmlファイルを作成、または、初期状態に戻しますか？\nはい　→　Y\nいいえ　→　Y以外の任意のキー");
-            if (Console.ReadLine() == "Y") BallsportList.CreateBallSports(wFilePath);
-
-            // BallSports.xmlファイルがなければ、強制終了させる。
-            if (!File.Exists(wFilePath)) {
-                Console.WriteLine("参照するファイルがないので、終了しました。");
-                return;
+            if (Console.ReadLine() == "Y") {
+                AddBallSport(wBasketball, wFilePath);
+                AddBallSport(wVolleyball, wFilePath);
+                AddBallSport(wBaseball, wFilePath);
             }
 
             // BallSports.xmlファイルを読み込んでwBallsportListsに格納する。
-            IEnumerable<BallsportList> wBallsportLists = BallsportList.ReadBallsportLists(wFilePath);
+            IEnumerable<BallSport> wReadedBallsports = ReadXFileToBallsports(wFilePath);
 
             // 1.の回答
             Console.WriteLine("\n問題1の回答（競技名とチームメンバー数の一覧）");
-            foreach (var wXList in wBallsportLists) {
-                Console.WriteLine($"競技名：{wXList.Name}→{wXList.Teammembers}人");
+            foreach (var wBallSport in wReadedBallsports) {
+                Console.WriteLine($"競技名：{wBallSport.Name}→{wBallSport.Teammembers}人");
             }
 
             // 2.の回答
             Console.WriteLine("\n問題2の回答（最初にプレイされた年の若い順）");
-            foreach (var wXList in wBallsportLists.OrderBy(x => (int)(x.Firstplayed))) {
-                Console.WriteLine($"{wXList.KanjiName}");
+            foreach (var wBallSport in wReadedBallsports.OrderBy(x => (int)(x.Firstplayed))) {
+                Console.WriteLine($"{wBallSport.KanjiName}");
             }
 
             // 3.の回答
             Console.WriteLine("\n問題3の回答（メンバー人数が最も多い競技名）");
-            foreach (var wXList in wBallsportLists) {
-                if (!(wXList.Teammembers == wBallsportLists.Max(x => x.Teammembers))) continue;
-                Console.WriteLine(wXList.Name);
+            foreach (var wBallSport in wReadedBallsports) {
+                if (!(wBallSport.Teammembers == wReadedBallsports.Max(x => x.Teammembers))) continue;
+                Console.WriteLine(wBallSport.Name);
             }
 
             // 4.の回答
             Console.WriteLine("\nBallSports.xmlファイルにサッカーのデータを追加しますか？\nはい　→　Y\nいいえ　→　Y以外の任意のキー");
             if (Console.ReadLine() == "Y") {
-                string[] wSoccerData = { "ballsport", "フットボール", "蹴球", "11", "1863" };
-                BallsportList.AddBallSport(wSoccerData, wFilePath);
+                BallSport wSoccer = new BallSport("フットボール", "蹴球", 11, 1863);
+                AddBallSport(wSoccer, wFilePath);
             }
         }
     }
