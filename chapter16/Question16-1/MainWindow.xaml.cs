bb911a61add4020638a5dd355decdb7a01c4bdf0
大平 強（OHDAIRA Tsuyoshi） <ohdaira@beingcorp.co.jp>
2023-03-06 12:47:08 +0900

    16章　章末問題
    【問題16-1】
    　編集したファイル（WPFアプリ）
    ・IInitializeWithWindow.cs
    ・MainWindow.xaml.cs
    　確認方法
    ①「ファイル選択」ボタンを押下するとファイルピッカーが出力します。
    問題文に沿って、ファイルピッカーは、.txtファイルのみ選択できるようにしています。
    ②txtファイルを選択して、画面にファイルの内容が出力されれば、確認完了です。
    
    【問題16-2】
    　編集したファイル（WindowsFormアプリ）
    ・From1.cs
    　確認方法
    ①（ディレクトリ選択）ボタンを押下するとディレクトリを選択するダイアログが出力します。
    ②ディレクトリを選択して、「OK」ボタンを押下すると「検索するディレクトリ」の下に選択したディレクトリのフルパスが出力します。
    ③「検索」ボタンを押下すると、検索が始まります。
    ④検索が完了すると「検索結果」の下に、「検索ワード」を含むファイル名がフルパスで出力します。
    並列処理と非並列処理の処理時間を比較するために、それぞれの結果を出力するようにしています。

---
+++ chapter16/Question16-1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace Question16_1 {
+
+    // 問題16.1
+    // .NEt Framework4.5以降のStreamReaderクラスには、非同期処理を実現するReadLineAsyncメソッドが追加されています。
+    // このメソッドを使い、テキストファイルを非同期で読み込むコードを書いてください。
+    // アプリケーションの形態は、WindowsFormsでもWPFでも好きなものを選択してください。
+
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, RoutedEventArgs e) {
+            var wLines = await ReadLines();
+            if (wLines != null) {
+                textBox1.Text = string.Join("\n", wLines);
+                label1.Content = "読み込んだテキストファイルの内容を表示しています。";
+            } else {
+                textBox1.Text = "";
+                label1.Content = "ファイル選択をキャンセルしました。";
+            }
+        }
+
+        /// <summary>
+        /// ファイルピッカーで選択したファイルを読み込むメソッド
+        /// </summary>
+        /// <returns>ファイルの内容</returns>
+        private async Task<IEnumerable<string>> ReadLines() {
+            var wOpenPicker = new FileOpenPicker {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = PickerLocationId.DocumentsLibrary,
+            };
+            // ウィンドウハンドルの指定
+            ((IInitializeWithWindow)(object)wOpenPicker).Initialize(new WindowInteropHelper(this).Handle);
+            wOpenPicker.FileTypeFilter.Add(".txt");
+            StorageFile wFile = await wOpenPicker.PickSingleFileAsync();
+
+            if (wFile == null) return null;
+
+            IList<string> wLines = await FileIO.ReadLinesAsync(wFile);
+
+            return wLines;
+        }
+    }
+}
---
+++ chapter16/Question16-2/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question16_2 {
+    /// <summary>
+    /// フォームクラス
+    /// </summary>
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            textBox3.Text = ChoiceDirectory();
+        }
+
+        /// <summary>
+        /// ディレクトリを選択するメソッド
+        /// </summary>
+        /// <returns>選択したディレクトリのフルパス</returns>
+        private static string ChoiceDirectory() {
+            FolderBrowserDialog wFbDialog = new FolderBrowserDialog();
+            wFbDialog.Description = "検索するディレクトリの選択";
+            wFbDialog.SelectedPath = @"C:";
+            if (wFbDialog.ShowDialog() == DialogResult.OK) {
+                return wFbDialog.SelectedPath;
+            } else {
+                return "キャンセルされました";
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e) {
+            if (textBox3.Text == "") return;
+            label8.Text = "検索中";
+            // 並列処理検索と非並列処理検索を、並列処理で行う
+            var wParallelRun = Task.Run(() => ParallelReadLine());
+            var wNormalRun = Task.Run(() => NormalReadLine());
+            var wParallelText = await wParallelRun;
+            var wNormalText = await wNormalRun;
+            textBox4.Text = wParallelText.Item1.ToString();
+            label7.Text = wParallelText.Item2;
+            textBox5.Text = wNormalText.Item1.ToString();
+            label13.Text = wNormalText.Item2;
+            label8.Text = "完了";
+        }
+
+        /// <summary>
+        /// 並列処理で検索するメソッド
+        /// </summary>
+        /// <returns>マッチしたパスと処理時間</returns>
+        private (StringBuilder, string) ParallelReadLine() {
+            var wTimer = Stopwatch.StartNew();
+            StringBuilder wPath = new StringBuilder();
+            IEnumerable<string> wFiles = Directory.EnumerateFiles(textBox3.Text, "*", SearchOption.AllDirectories);
+            foreach (string wFile in wFiles.AsParallel()
+                                        .Where(x => IsMatchText(x, textBox1.Text) && IsMatchText(x, textBox2.Text))) {
+                wPath.Append($"{wFile}\n");
+            };
+            wTimer.Stop();
+            return (wPath, wTimer.ElapsedMilliseconds.ToString());
+        }
+
+        /// <summary>
+        /// 非並列処理で検索するメソッド
+        /// </summary>
+        /// <returns>マッチしたパスと処理時間</returns>
+        private (StringBuilder, string) NormalReadLine() {
+            var wTimer = Stopwatch.StartNew();
+            StringBuilder wPath = new StringBuilder();
+            IEnumerable<string> wFiles = Directory.EnumerateFiles(textBox3.Text, "*", SearchOption.AllDirectories);
+            foreach (string wFile in wFiles.Where(x => IsMatchText(x, textBox1.Text) && IsMatchText(x, textBox2.Text))) {
+                wPath.Append($"{wFile}\n");
+            };
+            wTimer.Stop();
+            return (wPath, wTimer.ElapsedMilliseconds.ToString());
+        }
+
+        /// <summary>
+        /// 検索ワードにマッチするか判定するメソッド
+        /// </summary>
+        /// <param name="vPath">検索先のフルパス</param>
+        /// <param name="vText">検索ワード</param>
+        /// <returns>マッチしたかどうかの真偽</returns>
+        private bool IsMatchText(string vPath, string vText) => Regex.IsMatch(File.ReadAllText(vPath, Encoding.UTF8), $@"\b{vText}\b");
+
+        private void textBox4_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e) {
+
+        }
+
+        private void textBox4_TextChanged_1(object sender, EventArgs e) {
+            this.textBox4.ScrollBars = ScrollBars.Vertical;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e) {
+            this.textBox5.ScrollBars = ScrollBars.Vertical;
+        }
+
+        private void label9_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
